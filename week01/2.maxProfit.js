9986fd736a7df9ea94d00179ff9974555413caf1
Al Mamun <mamunhpath@hotmail.com>
2022-04-08 21:24:14 +0600

    maxprofit code added

--- week01/2.maxProfit.js
+++ week01/2.maxProfit.js
@@ -1,13 +1,10 @@
 var maxProfit = function (prices) {
+  let maxprofit = 0;
+  let minSofar = prices[0];
   for (let i = 0; i < prices.length; i++) {
-    console.log("i", prices[i]);
-    for (let j = 0; j < prices.length; j++) {
-      console.log("j", prices[j]);
-    }
+    minSofar = Math.min(prices[i], minSofar);
+    let profit = prices[i] - minSofar;
+    maxprofit = Math.max(maxprofit, profit);
   }
+  return maxprofit;
 };
-
-// console.log(maxProfit([7, 1, 5, 3, 6, 4]));
-// console.log(maxProfit([7, 6, 4, 3, 1]));
-// console.log(maxProfit([1, 2]));
-console.log(maxProfit([2, 4, 1]));
